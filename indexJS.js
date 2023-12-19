310b2cace09ea6b7d93c5464e586d4cfc0de0860
700Hacker007 <145757045+700Hacker007@users.noreply.github.com>
2023-12-19 13:26:20 +0100

    1.2 Version Bug fixed and new feature name 2 in 1. Now you can have two task in one.

--- indexJS.js
+++ indexJS.js
@@ -13,8 +13,9 @@ document.addEventListener('DOMContentLoaded', function () {
 
     function addTask(taskText) {
         const listItem = document.createElement('li');
+        listItem.draggable = true;
         listItem.innerHTML = `
-            <div class="task">
+            <div class="task" draggable="true" ondragstart="drag(event)">
                 <span class="newitem">${taskText}</span>
                 <button class="deleteBtn">Delete</button>
             </div>`;
@@ -25,4 +26,39 @@ document.addEventListener('DOMContentLoaded', function () {
             listItem.remove();
         });
     }
+
+    window.drag = function (event) {
+        event.dataTransfer.setData('text', event.target.outerHTML);
+        event.target.style.opacity = '0.4';
+    };
+
+    window.allowDrop = function (event) {
+        event.preventDefault();
+    };
+
+    window.drop = function (event) {
+        event.preventDefault();
+        const data = event.dataTransfer.getData('text');
+        const draggedItem = document.createElement('div');
+        draggedItem.innerHTML = data;
+
+        // Check if the drop target is a valid next position
+        const dropTarget = event.target.closest('.task');
+        if (dropTarget) {
+            const tasks = Array.from(tasksList.querySelectorAll('.task'));
+            const dropIndex = tasks.indexOf(dropTarget);
+
+            // Check if the dragged item is being dropped next to another item
+            if (dropIndex !== -1) {
+                const draggedIndex = tasks.indexOf(draggedItem.firstChild);
+                const diff = Math.abs(dropIndex - draggedIndex);
+
+                if (diff === 1 || diff === 0) {
+                    event.target.appendChild(draggedItem.firstChild);
+                }
+            }
+        }
+
+        draggedItem.firstChild.style.opacity = '1';
+    };
 });
